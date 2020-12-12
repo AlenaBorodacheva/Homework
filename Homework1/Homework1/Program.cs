8d88905c8a1dedefa78ef8788963ccf970f9542b
AlenaBorodacheva <alien_borodach@mail.ru>
2020-12-13 00:04:09 +0300

    файл добавлен

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            string name = Console.ReadLine();
+            DateTime date = DateTime.Now;
+            Console.WriteLine($"Привет, {name}, сегодня {date.ToString("dd.MM.yyyy")}.");
+            Console.ReadLine();
+        }
+    }
+}

5440a494d46fa34ed93af34e7050374e8a6de734
AlenaBorodacheva <alien_borodach@mail.ru>
2020-12-29 18:45:29 +0300

    Выполнила из пятого задания только 1, 2 и 3 задачу. Остальные доделаю в следующий раз.

---
+++ 1. File txt/1. File txt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _1.File_txt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "text.txt";
+            Console.WriteLine("Введите данные:");
+            while (true)
+            {
+                string data = Console.ReadLine();
+                File.AppendAllText(filename, data);
+                File.AppendAllText(filename, Environment.NewLine);
+                Console.WriteLine("Для продолжения нажмите любую клавишу. Для выхода нажмите Esc.");
+                if (Console.ReadKey(true).Key != ConsoleKey.Escape)
+                    Console.WriteLine("Введите следующее значение:");
+                else
+                    break;
+            }
+        }
+    }
+}
---
+++ 1. File txt/4.Tree_directory/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _4.Tree_directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Without_Recursion();
+            //With_Recursion();
+        }
+        static void Without_Recursion()
+        {
+            string dirName = "E:\\Заставки\\";
+            string filename = "text.txt";
+
+            if (Directory.Exists(dirName))
+            {
+                File.AppendAllText(filename, "Подкаталоги:");
+                File.AppendAllText(filename, Environment.NewLine);
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    File.AppendAllText(filename, s);
+                    File.AppendAllText(filename, Environment.NewLine);
+                }
+
+                File.AppendAllText(filename, "Файлы:");
+                File.AppendAllText(filename, Environment.NewLine);
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    File.AppendAllText(filename, s);
+                    File.AppendAllText(filename, Environment.NewLine);
+                }
+            }
+
+        }
+        static void With_Recursion()
+        {
+            string dirName = "E:\\Заставки\\";
+            string filename = "text.txt";
+            foreach (string s in Recursion(dirName, filename))
+            {
+                File.AppendAllText(filename, s);
+                File.AppendAllText(filename, Environment.NewLine);
+            }
+        }
+        static string[] Recursion(string dirName, string filename)
+        {
+            if (Directory.Exists(dirName))
+            {
+                File.AppendAllText(filename, "Подкаталоги:");
+                File.AppendAllText(filename, Environment.NewLine);
+                string[] dirs = Directory.GetDirectories(dirName);
+                return dirs;
+            }
+            else
+            {
+                return Recursion(dirName, filename);
+            }
+        }
+    }
+}

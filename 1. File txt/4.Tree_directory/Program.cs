08aa9f2876e06cee1b71f36b1737105255a59d3a
AlenaBorodacheva <alien_borodach@mail.ru>
2021-01-02 11:44:57 +0300

    добавлена четвертая задача к пятому домашнему заданию.

--- 1. File txt/4.Tree_directory/Program.cs
+++ 1. File txt/4.Tree_directory/Program.cs
@@ -11,58 +11,85 @@ namespace _4.Tree_directory
     {
         static void Main(string[] args)
         {
-            Without_Recursion();
-            //With_Recursion();
+            //Without_Recursion();
+            With_Recursion();
         }
         static void Without_Recursion()
         {
-            string dirName = "E:\\Заставки\\";
+            string dirName = "E:\\test\\";
             string filename = "text.txt";
-
-            if (Directory.Exists(dirName))
-            {
-                File.AppendAllText(filename, "Подкаталоги:");
-                File.AppendAllText(filename, Environment.NewLine);
-                string[] dirs = Directory.GetDirectories(dirName);
-                foreach (string s in dirs)
-                {
-                    File.AppendAllText(filename, s);
-                    File.AppendAllText(filename, Environment.NewLine);
-                }
-
-                File.AppendAllText(filename, "Файлы:");
-                File.AppendAllText(filename, Environment.NewLine);
-                string[] files = Directory.GetFiles(dirName);
-                foreach (string s in files)
-                {
-                    File.AppendAllText(filename, s);
-                    File.AppendAllText(filename, Environment.NewLine);
-                }
-            }
-
-        }
-        static void With_Recursion()
-        {
-            string dirName = "E:\\Заставки\\";
-            string filename = "text.txt";
-            foreach (string s in Recursion(dirName, filename))
+            
+            string[] dirs = Directory.GetDirectories(dirName);
+            foreach (string s in dirs)
             {
                 File.AppendAllText(filename, s);
                 File.AppendAllText(filename, Environment.NewLine);
             }
-        }
-        static string[] Recursion(string dirName, string filename)
-        {
-            if (Directory.Exists(dirName))
+
+            string[] files = Directory.GetFiles(dirName);
+            foreach (string s in files)
             {
-                File.AppendAllText(filename, "Подкаталоги:");
+                File.AppendAllText(filename, s);
                 File.AppendAllText(filename, Environment.NewLine);
-                string[] dirs = Directory.GetDirectories(dirName);
-                return dirs;
             }
-            else
+
+            string[] allDirs;
+            allDirs = Directory.GetDirectories(dirName, "*", SearchOption.AllDirectories);
+            string[] subFiles;
+            string[] subDirs;
+
+            for (int i = 0; i < allDirs.Length; i++)
             {
-                return Recursion(dirName, filename);
+                subFiles = Directory.GetFiles(allDirs[i]);
+                subDirs = Directory.GetDirectories(allDirs[i]);
+                foreach (string s in subDirs)
+                {
+                    File.AppendAllText(filename, s);
+                    File.AppendAllText(filename, Environment.NewLine);
+                }
+                foreach (string s in subFiles)
+                {
+                    File.AppendAllText(filename, s);
+                    File.AppendAllText(filename, Environment.NewLine);
+                }
+            }
+        }
+        
+        static void With_Recursion()
+        {
+            string dirName = "E:\\test\\";
+            string filename = "text2.txt";
+
+            List<string> ls1 = GetRecursFiles(dirName);
+            foreach (string s in ls1)
+            {
+                File.AppendAllText(filename, s);
+                File.AppendAllText(filename, Environment.NewLine);
+            }
+            
+            List<string> GetRecursFiles(string start_path)
+            {
+                List<string> ls = new List<string>();
+                try
+                {
+                    string[] folders = Directory.GetDirectories(start_path);
+                    foreach (string s in folders)
+                    {
+                        ls.Add(s);
+                        ls.AddRange(GetRecursFiles(s));
+                    }
+                    string[] files = Directory.GetFiles(start_path);
+                    foreach (string s in files)
+                    {
+                        ls.Add(s);
+                    }
+                }
+                catch (System.Exception e)
+                {
+                    File.AppendAllText(filename, e.Message);
+                    File.AppendAllText(filename, Environment.NewLine);
+                }
+                return ls;
             }
         }
     }
